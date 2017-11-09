670f7f889db639c66745d4b287cd517c0cef352b
Alberto Salazar <alberto.salazar@ac-techsolutions.com>
2017-11-08 21:43:21 -0500

    New App
    New App

---
+++ HelloWorld/platforms/ios/build/HelloWorld.build/Debug-iphonesimulator/HelloWorld.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,11 @@
+dependencies: \
+  /Users/alberto/TRABAJO/REPOSITORIES/native-script-example/HelloWorld/platforms/ios/HelloWorld/HelloWorld-Prefix.pch \
+  /Users/alberto/TRABAJO/REPOSITORIES/native-script-example/HelloWorld/platforms/ios/internal/main.m \
+  /Users/alberto/TRABAJO/REPOSITORIES/native-script-example/HelloWorld/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
+  /Users/alberto/TRABAJO/REPOSITORIES/native-script-example/HelloWorld/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntimeInstrumentation.h \
+  /Users/alberto/TRABAJO/REPOSITORIES/native-script-example/HelloWorld/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
+  /Users/alberto/TRABAJO/REPOSITORIES/native-script-example/HelloWorld/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
+  /Users/alberto/TRABAJO/REPOSITORIES/native-script-example/HelloWorld/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
+  /Users/alberto/TRABAJO/REPOSITORIES/native-script-example/HelloWorld/platforms/ios/internal/TNSExceptionHandler.h \
+  /Users/alberto/TRABAJO/REPOSITORIES/native-script-example/HelloWorld/platforms/ios/internal/TNSDebugging.h \
+  /Users/alberto/TRABAJO/REPOSITORIES/native-script-example/HelloWorld/platforms/ios/internal/TKLiveSync/include/TKLiveSync.h
